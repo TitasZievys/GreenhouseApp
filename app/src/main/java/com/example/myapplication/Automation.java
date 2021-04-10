4d9a0f11ea07354489eea7a0643b0bc382f56448
TitasZievys <82270284+TitasZievys@users.noreply.github.com>
2021-04-10 16:32:52 +0300

    automation page open

--- app/src/main/java/com/example/myapplication/Automation.java
+++ app/src/main/java/com/example/myapplication/Automation.java
@@ -1,4 +1,14 @@
 package com.example.myapplication;
 
-public class Automation {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Automation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automation);
+    }
 }
